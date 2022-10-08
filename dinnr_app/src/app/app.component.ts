bc789374b1a6d855605a6eeacb45ba6e2301c1f5
LindseyAllen1 <allen.2660@osu.edu>
2022-10-08 13:45:02 -0400

    filling in Filled in app component files with code from tutorial

--- dinnr_app/src/app/app.component.ts
+++ dinnr_app/src/app/app.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'dinnr_app';
+  title = 'google-places-autocomplete';
+  userAddress: string = ''
+  userLatitude: string = ''
+  userLongitude: string = ''
+
+
+  handleAddressChange(address: any) {
+    this.userAddress = address.formatted_address
+    this.userLatitude = address.geometry.location.lat()
+    this.userLongitude = address.geometry.location.lng()
+  }
 }
